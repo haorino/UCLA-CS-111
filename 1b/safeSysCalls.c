bafe2d33674ba4215f6e297706e279a0bc7ba7eb
Siddharth Joshi <sjoshi804@gmail.com>
2018-10-12 12:45:56 -0700

    Initial commit for project 1B

---
+++ 1b/safeSysCalls.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <errno.h>
+#include "safeSysCalls.h"
+
+/* --- System calls with error handling --- */
+int safeRead (int fd, char* buffer, ssize_t size)
+{
+    int status = read(fd, buffer, size);
+    if (status < 0)
+    {
+        fprintf(stderr, "Unable to read from STDIO %s", strerror(errno));
+        exit(1);
+    }
+
+    return status;
+}
+
+int safeWrite (int fd, char* buffer, ssize_t size)
+{
+    int status = write(fd, buffer, size);
+    if (status != size)
+    {
+        fprintf(stderr, "Unable to write to STDOUT %s", strerror(errno)); 
+        exit(1);
+    }
+
+    return status;
+}
+
+void safeDup2(int original_fd, int new_fd)
+{
+    if (dup2(original_fd, new_fd) < 0)
+    {
+        fprintf(stderr, "Dup2 failed: %s", strerror(errno));
+        exit(1);
+    }
+}
+
+void safeClose(int fd)
+{
+    if (close(fd) < 0)
+    {
+        fprintf(stderr, "Error closing file: %s", strerror(errno));
+        exit(1);
+    }
+}
---
+++ 1b/safeSysCalls.h
@@ -0,0 +1,16 @@
+#ifndef SAFE_SYS_CALLS
+#define SAFE_SYS_CALLS
+
+//Safe System Calls Library Header File
+#include <stdio.h>
+/* --- System calls with error handling --- */
+
+int safeRead (int fd, char* buffer, ssize_t size);
+
+int safeWrite (int fd, char* buffer, ssize_t size);
+
+void safeDup2(int original_fd, int new_fd);
+
+void safeClose(int fd);
+
+#endif
