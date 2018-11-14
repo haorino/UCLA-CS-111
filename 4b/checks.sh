b27ecde422b0e09de898529eb9baef8a8daaba9c
Siddharth Joshi <sjoshi804@gmail.com>
2018-11-14 11:10:18 -0800

    Placeholder checks created

---
+++ 4b/checks.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo Smoke Tests started:
+
+#Check Bogus Argument
+
+#Check LogFile Creation
+
+#Check Logging of Commands in LogFile
+
+#If reaches here, all tests passed
+echo Passed all tests.
